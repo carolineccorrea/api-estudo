3c5fa8a20f82364ca69b135472d5be6811214d71
Your Name <you@example.com>
2020-11-12 04:42:41 -0300

    a

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -3,13 +3,13 @@ export class SignUpController {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
-        body: new Error('Parametros faltando: nome')
+        body: new Error(' Parametros faltando: nome ')
       }
     }
     if (!httpRequest.body.email) {
       return {
         statusCode: 400,
-        body: new Error('Parametros faltando: email')
+        body: new Error(' Parametros faltando: email ')
       }
     }
   }
