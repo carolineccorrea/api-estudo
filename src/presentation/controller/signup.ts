4039101ab3b3c006b7bd2c7acea506eee679f5e8
Your Name <you@example.com>
2020-11-16 17:05:54 -0300

    add classe producao

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,15 +1,14 @@
-export class SignUpController {
-  handle (httpRequest: any): any {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new Error('Parametros faltando: nome')
-      }
-    }
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new Error('Parametros faltando: email')
+import { HttpResponse, HttpRequest } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
+import { Controller } from '../protocols/controller'
+
+export class SignUpController implements Controller {
+  handle (httpRequest: HttpRequest): HttpResponse {
+    const requiredFields = ['name','email','password','passwordConfirm']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
       }
     }
   }
