551d4aa1fa81ba4e60d2ecdd863dfa99a1d50672
Your Name <you@example.com>
2020-11-18 03:08:26 -0300

    subindo arquivos de testes

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -2,6 +2,7 @@ import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-param-error'
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { EmailValidator } from '../protocols/email-validator'
+import { ServerError } from '../errors/server-error'
 
 interface SutTypes {
   sut: SignUpController
@@ -97,4 +98,42 @@ test('deve retornar 400 se o email for invalido', () => {
   const httpResponse = sut.handle(httpResquest)
   expect(httpResponse.statusCode).toBe(400)
   expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+})
+
+test('garante que o EmailValidator vai ser chamado com o email correto', () => {
+  const { sut, emailValidatorStub } = makeSut()
+  const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+
+  const httpResquest = {
+    body: {
+      name: 'exemplo@email.com',
+      email: 'fulano@email.com',
+      password: 'exemplo12345',
+      passwordConfirm: 'exemplo12345'
+    }
+  }
+  sut.handle(httpResquest)
+  expect(isValidSpy).toHaveBeenCalledWith('fulano@email.com')
+})
+
+test('deve retornar 500 se o emailValidator retornar uma exceção', () => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      throw new Error()
+    }
+  }
+  const emailValidatorStub = new EmailValidatorStub()
+  const sut = new SignUpController(emailValidatorStub)
+
+  const httpResquest = {
+    body: {
+      name: 'exemplo@email.com',
+      email: 'fulano@email.com',
+      password: 'exemplo12345',
+      passwordConfirm: 'exemplo12345'
+    }
+  }
+  const httpResponse = sut.handle(httpResquest)
+  expect(httpResponse.statusCode).toBe(500)
+  expect(httpResponse.body).toEqual(new ServerError())
 })
